059a2b87ae027497e73abc4d6d7d203a0590dc18
Caiml <826288248@qq.com>
2021-04-11 11:23:56 +0800

    2-2 init

---
+++ src/test/java/com/jiawa/wiki/WikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiawa.wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
