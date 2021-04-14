ad97d7b943a5bd54f48aa3c4a476592fd4d98837
Caiml <826288248@qq.com>
2021-04-14 17:09:18 +0800

    3-5 集成mybatis generator，生成demo表代码

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -7,3 +7,13 @@ create table `test` (
 ) engine=innodb default charset=utf8mb4 comment='测试';
 
 insert into `test` (id, name, password) values (1, '测试', 'password');
+
+
+drop table if exists `demo`;
+create table `demo` (
+  `id` bigint not null comment 'id',
+  `name` varchar(50) comment '名称',
+  primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='测试';
+
+insert into `demo` (id, name) values (1, '测试');
