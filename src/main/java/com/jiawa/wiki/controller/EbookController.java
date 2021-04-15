7f20af874f576b3cff483127e10ef7bce08df36c
Caiml <826288248@qq.com>
2021-04-15 21:19:35 +0800

    3-6 完成电子书列表查询接口

---
+++ src/main/java/com/jiawa/wiki/controller/EbookController.java
@@ -0,0 +1,23 @@
+package com.jiawa.wiki.controller;
+
+import com.jiawa.wiki.domain.Ebook;
+import com.jiawa.wiki.service.EbookService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/ebook")
+public class EbookController {
+
+    @Resource
+    private EbookService ebookService;
+
+    @GetMapping("/list")
+    public List<Ebook> list() {
+        return ebookService.list();
+    }
+}
---
+++ src/main/java/com/jiawa/wiki/service/EbookService.java
@@ -0,0 +1,18 @@
+package com.jiawa.wiki.service;
+
+import com.jiawa.wiki.domain.Ebook;
+import com.jiawa.wiki.mapper.EbookMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class EbookService {
+    @Resource
+    private EbookMapper ebookMapper;
+
+    public List<Ebook> list() {
+        return ebookMapper.selectByExample(null);
+    }
+}
