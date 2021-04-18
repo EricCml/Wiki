54268d47de7b5bf4c5f0ec1d231ef9eb9e9c18af
Caiml <826288248@qq.com>
2021-04-18 14:08:34 +0800

    4-7 制作the-footer组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,20 +2,20 @@
   <a-layout id="components-layout-demo-top-side-2">
     <the-header></the-header>
     <router-view/>
-    <a-layout-footer style="text-align: center">
-      甲蛙电子书
-    </a-layout-footer>
+    <the-footer></the-footer>
   </a-layout>
 </template>
 
 <script lang="ts">
 import {defineComponent} from 'vue';
 import TheHeader from '@/components/the-header.vue';
+import TheFooter from '@/components/the-footer.vue';
 
 export default defineComponent({
   name: 'app',
   components: {
     TheHeader,
+    TheFooter
   },
 });
 </script>
---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    甲蛙电子书
+  </a-layout-footer>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+  name: 'the-footer'
+});
+</script>
