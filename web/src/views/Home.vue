9d8ec1baa23506dd76f29a3fe598672733c89ba7
Caiml <826288248@qq.com>
2021-04-19 08:59:56 +0800

    5-2 使用axios调用后端接口/ebook/list

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -3,8 +3,6 @@
     <a-layout-sider width="200" style="background: #fff">
       <a-menu
           mode="inline"
-          v-model:selectedKeys="selectedKeys2"
-          v-model:openKeys="openKeys"
           :style="{ height: '100%', borderRight: 0 }"
       >
         <a-sub-menu key="sub1">
@@ -46,8 +44,15 @@
 
 <script lang="ts">
 import {defineComponent} from 'vue';
+import axios from "axios";
 
 export default defineComponent({
   name: 'Home',
+  setup() {
+    console.log("setup");
+    axios.get("http://localhost:8880/ebook/list?name=Spring").then((response) => {
+      console.log(response);
+    })
+  }
 });
 </script>
