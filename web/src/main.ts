9dc79df07b81de64613697cf492b24b226443556
Caiml <826288248@qq.com>
2021-04-20 11:03:01 +0800

    5-5 修改axios的baseUrl读配置文件

--- web/src/main.ts
+++ web/src/main.ts
@@ -5,6 +5,9 @@ import store from './store'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 import * as Icons from '@ant-design/icons-vue';
+import axios from "axios";
+
+axios.defaults.baseURL = process.env.VUE_APP_SERVER;
 
 const app = createApp(App);
 app.use(store).use(router).use(Antd).mount('#app');
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -86,8 +86,8 @@ export default defineComponent({
     const ebooks1 = reactive({books: []});
 
     onMounted(() => {
-      console.log("onMounted 1234");
-      axios.get(process.env.VUE_APP_SERVER+"/ebook/list").then((response) => {
+      console.log("onMounted");
+      axios.get("/ebook/list").then((response) => {
         const data = response.data;
         ebooks.value = data.content;
         ebooks1.books = data.content;
