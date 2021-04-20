7c6945047ad773616c219f17c554f69684ea4f0b
Caiml <826288248@qq.com>
2021-04-20 10:52:51 +0800

    5-5 axios请求域改为读配置文件

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -86,8 +86,8 @@ export default defineComponent({
     const ebooks1 = reactive({books: []});
 
     onMounted(() => {
-      console.log("onMounted");
-      axios.get("http://127.0.0.1:8880/ebook/list").then((response) => {
+      console.log("onMounted 1234");
+      axios.get(process.env.VUE_APP_SERVER+"/ebook/list").then((response) => {
         const data = response.data;
         ebooks.value = data.content;
         ebooks1.books = data.content;
