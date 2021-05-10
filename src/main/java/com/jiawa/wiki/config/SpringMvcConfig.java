2c076c432e3e5918ba14aa6803b02b7c3f2dfb50
Caiml <826288248@qq.com>
2021-05-10 23:08:03 +0800

    5-8 增加拦截器，打印接口耗时

---
+++ src/main/java/com/jiawa/wiki/config/SpringMvcConfig.java
@@ -0,0 +1,20 @@
+package com.jiawa.wiki.config;
+
+import com.jiawa.wiki.interceptor.LogInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LogInterceptor logInterceptor;
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor)
+                .addPathPatterns("/**").excludePathPatterns("/login");  // addPathPatterns()，要拦截请求;excludePathPatterns()，排除请求，不拦截
+    }
+}
