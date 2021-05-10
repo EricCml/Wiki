786af5a7ce7b6c86338d264779a628b9a6f3446c
Caiml <826288248@qq.com>
2021-05-10 23:01:00 +0800

    5-6 配置axios拦截器打印请求参数和返回参数

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -81,17 +81,14 @@ for (let i = 0; i < 23; i++) {
 export default defineComponent({
   name: 'Home',
   setup() {
-    console.log("setup");
     const ebooks = ref();
     const ebooks1 = reactive({books: []});
 
     onMounted(() => {
-      console.log("onMounted");
       axios.get("/ebook/list").then((response) => {
         const data = response.data;
         ebooks.value = data.content;
         ebooks1.books = data.content;
-        console.log(response);
       });
     });
 
