8f2fcb9690a28e512c6ff7f0da0f8218b117f5bf
EricCml <826288248@qq.com>
2021-09-14 15:01:23 +0800

    7-6 修复BUG，编辑保存重新加载数据后，再点编辑，则列表显示的还是编辑前的数据

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -140,6 +140,8 @@ export default defineComponent({
      **/
     const handleQuery = (params: any) => {
       loading.value = true;
+      // 如果不清空现有数据，则编辑保存重新加载数据后，再点编辑，则列表显示的还是编辑前的数据
+      ebooks.value = [];
       axios.get("/ebook/list", {
         params: {
           page: params.page,
