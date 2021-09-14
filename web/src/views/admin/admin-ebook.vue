28a5ce6a80937d8f38df5bff9714e0632e4569f5
EricCml <826288248@qq.com>
2021-09-14 14:52:40 +0800

    7-6 分类选择改为级联选择组件

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -66,11 +66,12 @@
       <a-form-item label="名称">
         <a-input v-model:value="ebook.name"/>
       </a-form-item>
-      <a-form-item label="分类一">
-        <a-input v-model:value="ebook.category1Id"/>
-      </a-form-item>
-      <a-form-item label="分类二">
-        <a-input v-model:value="ebook.category2Id"/>
+      <a-form-item label="分类">
+        <a-cascader
+            v-model:value="categoryIds"
+            :field-names="{ label: 'name', value: 'id', children: 'children' }"
+            :options="level1"
+        />
       </a-form-item>
       <a-form-item label="描述">
         <a-input v-model:value="ebook.description" type="textarea"/>
@@ -150,7 +151,6 @@ export default defineComponent({
       }).then((response) => {
         loading.value = false;
         const data = response.data;
-
         if (data.success) {
           ebooks.value = data.content.list;
 
@@ -175,11 +175,17 @@ export default defineComponent({
     };
 
     // -------- 表单 ---------
-    const ebook = ref({});
+    /**
+     * 数组，[100, 101]对应：前端开发 / Vue
+     */
+    const categoryIds = ref();
+    const ebook = ref();
     const modalVisible = ref(false);
     const modalLoading = ref(false);
     const handleModalOk = () => {
       modalLoading.value = true;
+      ebook.value.category1Id = categoryIds.value[0];
+      ebook.value.category2Id = categoryIds.value[1];
       axios.post("/ebook/save", ebook.value).then((response) => {
         modalLoading.value = false;
         const data = response.data; // data = commonResp
@@ -194,8 +200,6 @@ export default defineComponent({
         } else {
           message.error(data.message);
         }
-        ebooks.value = data.content.list;
-
       });
     };
 
@@ -205,6 +209,7 @@ export default defineComponent({
     const edit = (record: any) => {
       modalVisible.value = true;
       ebook.value = Tool.copy(record);
+      categoryIds.value = [ebook.value.category1Id, ebook.value.category2Id]
     };
 
     /**
@@ -215,26 +220,46 @@ export default defineComponent({
       ebook.value = {};
     };
 
-    /**
-     * 删除
-     */
     const handleDelete = (id: number) => {
-      axios.delete("/ebook/delete/" + id).then((reponse) => {
-        const data = reponse.data; // data = commonResp
+      axios.delete("/ebook/delete/" + id).then((response) => {
+        const data = response.data; // data = commonResp
         if (data.success) {
           // 重新加载列表
           handleQuery({
             page: pagination.value.current,
             size: pagination.value.pageSize,
-          })
+          });
         }
-      })
-    }
+      });
+    };
+
+    const level1 = ref();
+    /**
+     * 查询所有分类
+     **/
+    const handleQueryCategory = () => {
+      loading.value = true;
+      axios.get("/category/all").then((response) => {
+        loading.value = false;
+        const data = response.data;
+        if (data.success) {
+          const categorys = data.content;
+          console.log("原始数组：", categorys);
+
+          level1.value = [];
+          level1.value = Tool.array2Tree(categorys, 0);
+          console.log("树形结构：", level1.value);
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
 
     onMounted(() => {
+      handleQueryCategory();
       handleQuery({
         page: 1,
-        size: pagination.value.pageSize
+        size: pagination.value.pageSize,
       });
     });
 
@@ -254,7 +279,10 @@ export default defineComponent({
       modalVisible,
       modalLoading,
       handleModalOk,
-      handleDelete,
+      categoryIds,
+      level1,
+
+      handleDelete
     }
   }
 });
