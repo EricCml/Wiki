f993fed7642fe89e1bdcb284700855c60903aab2
EricCml <826288248@qq.com>
2021-09-19 21:07:20 +0800

    8-9 让表格树默认展开

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -20,12 +20,14 @@
             </a-form>
           </p>
           <a-table
+              v-if="level1.length > 0"
               :columns="columns"
               :row-key="record => record.id"
               :data-source="level1"
               :loading="loading"
               :pagination="false"
               size="small"
+              :default-expand-all-rows="true"
           >
             <template #name="{ text, record }">
               {{ record.sort }} {{ text }}
@@ -150,6 +152,7 @@ export default defineComponent({
      * }]
      */
     const level1 = ref(); // 一级文档树，children属性就是二级文档
+    level1.value = [];
 
     /**
      * 数据查询
