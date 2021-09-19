08b08173ed424ff8d0beb32dfeb8ff00de56f7bd
EricCml <826288248@qq.com>
2021-09-19 22:18:24 +0800

    8-11 增加保存文档成功提示 编辑和新增的时候，先清空富文本框

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -198,7 +198,8 @@ export default defineComponent({
         modalLoading.value = false;
         const data = response.data; // data = commonResp
         if (data.success) {
-          modalVisible.value = false;
+          // modalVisible.value = false;
+          message.success("保存成功！");
 
           // 重新加载列表
           handleQuery();
@@ -291,6 +292,8 @@ export default defineComponent({
      * 编辑
      */
     const edit = (record: any) => {
+      // 清空富文本框
+      editor.txt.html("");
       modalVisible.value = true;
       doc.value = Tool.copy(record);
       handleQueryContent();
@@ -308,6 +311,8 @@ export default defineComponent({
      * 新增
      */
     const add = () => {
+      // 清空富文本框
+      editor.txt.html("");
       modalVisible.value = true;
       doc.value = {
         ebookId: route.query.ebookId
