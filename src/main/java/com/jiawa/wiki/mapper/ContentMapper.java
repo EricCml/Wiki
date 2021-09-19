b25ec47f32feda195d207b08d56205486401e68c
EricCml <826288248@qq.com>
2021-09-19 16:22:32 +0800

    8-8 文档内容表设计与代码生成

---
+++ src/main/java/com/jiawa/wiki/mapper/ContentMapper.java
@@ -0,0 +1,34 @@
+package com.jiawa.wiki.mapper;
+
+import com.jiawa.wiki.domain.Content;
+import com.jiawa.wiki.domain.ContentExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface ContentMapper {
+    long countByExample(ContentExample example);
+
+    int deleteByExample(ContentExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Content record);
+
+    int insertSelective(Content record);
+
+    List<Content> selectByExampleWithBLOBs(ContentExample example);
+
+    List<Content> selectByExample(ContentExample example);
+
+    Content selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Content record, @Param("example") ContentExample example);
+
+    int updateByExampleWithBLOBs(@Param("record") Content record, @Param("example") ContentExample example);
+
+    int updateByExample(@Param("record") Content record, @Param("example") ContentExample example);
+
+    int updateByPrimaryKeySelective(Content record);
+
+    int updateByPrimaryKeyWithBLOBs(Content record);
+}
