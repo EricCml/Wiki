2f68048f24a95b97a33b121743ad1af0e67a36ec
EricCml <826288248@qq.com>
2021-09-20 11:00:40 +0800

    8-12 打开文档页面时，默认选中第一个节点

--- web/src/views/doc.vue
+++ web/src/views/doc.vue
@@ -9,6 +9,7 @@
               @select="onSelect"
               :replaceFields="{title: 'name', key: 'id', value: 'id'}"
               :defaultExpandAll="true"
+              :defaultSelectedKeys="defaultSelectedKeys"
           >
           </a-tree>
         </a-col>
@@ -33,6 +34,8 @@ export default defineComponent({
     const route = useRoute();
     const docs = ref();
     const html = ref();
+    const defaultSelectedKeys = ref();
+    defaultSelectedKeys.value = [];
 
     /**
      * 一级文档树，children属性就是二级文档
@@ -49,6 +52,20 @@ export default defineComponent({
     level1.value = [];
 
     /**
+     * 内容查询
+     **/
+    const handleQueryContent = (id: number) => {
+      axios.get("/doc/find-content/" + id).then((response) => {
+        const data = response.data;
+        if (data.success) {
+          html.value = data.content;
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
+    /**
      * 数据查询
      **/
     const handleQuery = () => {
@@ -59,20 +76,11 @@ export default defineComponent({
 
           level1.value = [];
           level1.value = Tool.array2Tree(docs.value, 0);
-        } else {
-          message.error(data.message);
-        }
-      });
-    };
 
-    /**
-     * 内容查询
-     **/
-    const handleQueryContent = (id: number) => {
-      axios.get("/doc/find-content/" + id).then((response) => {
-        const data = response.data;
-        if (data.success) {
-          html.value = data.content;
+          if (Tool.isNotEmpty(level1)) {
+            defaultSelectedKeys.value = [level1.value[0].id];
+            handleQueryContent(level1.value[0].id);
+          }
         } else {
           message.error(data.message);
         }
@@ -95,7 +103,8 @@ export default defineComponent({
     return {
       level1,
       html,
-      onSelect
+      onSelect,
+      defaultSelectedKeys,
     }
   }
 });
