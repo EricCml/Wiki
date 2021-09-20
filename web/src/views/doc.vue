7f5e7b9abf42b5975f19c73ce24e093ed358d432
EricCml <826288248@qq.com>
2021-09-20 10:33:11 +0800

    8-12 点击文档节点时，加载并显示文档内容

--- web/src/views/doc.vue
+++ web/src/views/doc.vue
@@ -13,6 +13,7 @@
           </a-tree>
         </a-col>
         <a-col :span="18">
+          <div :innerHTML="html"></div>
         </a-col>
       </a-row>
     </a-layout-content>
@@ -31,6 +32,7 @@ export default defineComponent({
   setup() {
     const route = useRoute();
     const docs = ref();
+    const html = ref();
 
     /**
      * 一级文档树，children属性就是二级文档
@@ -63,12 +65,37 @@ export default defineComponent({
       });
     };
 
+    /**
+     * 内容查询
+     **/
+    const handleQueryContent = (id: number) => {
+      axios.get("/doc/find-content/" + id).then((response) => {
+        const data = response.data;
+        if (data.success) {
+          html.value = data.content;
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
+    const onSelect = (selectedKeys: any, info: any) => {
+      console.log('selected', selectedKeys, info);
+      if (Tool.isNotEmpty(selectedKeys)) {
+        // 加载内容
+        handleQueryContent(selectedKeys[0]);
+      }
+    };
+
+
     onMounted(() => {
       handleQuery();
     });
 
     return {
       level1,
+      html,
+      onSelect
     }
   }
 });
