ab3f72ddc7895cf3fe80c1f33e6787977a82b7ec
EricCml <826288248@qq.com>
2021-09-21 19:42:12 +0800

    9-6 修改的时候，不修改密码

--- src/main/java/com/jiawa/wiki/service/UserService.java
+++ src/main/java/com/jiawa/wiki/service/UserService.java
@@ -84,6 +84,7 @@ public class UserService {
         } else {
             // 更新
             user.setLoginName(null);
+            user.setPassword(null);
             userMapper.updateByPrimaryKeySelective(user);
         }
     }
