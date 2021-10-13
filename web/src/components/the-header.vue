4c662a535105b98b135d4acb679541c168ba3387
EricCml <826288248@qq.com>
2021-10-13 16:53:40 +0800

    9-13 增加前端表单校验

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -47,14 +47,16 @@
         @ok="login"
     >
       <a-form
+          ref="formRef"
           :model="loginUser"
           :label-col="{ span: 6 }"
           :wrapper-col="{ span: 18 }"
+          :rules="rules"
       >
-        <a-form-item label="登录名">
+        <a-form-item has-feedback label="登录名" name="loginName">
           <a-input v-model:value="loginUser.loginName"/>
         </a-form-item>
-        <a-form-item label="密码">
+        <a-form-item has-feedback label="密码" name="password">
           <a-input v-model:value="loginUser.password" type="password"/>
         </a-form-item>
       </a-form>
@@ -63,49 +65,98 @@
 </template>
 
 <script lang="ts">
-import {computed, defineComponent, ref} from 'vue';
+import {computed, defineComponent, ref, reactive, UnwrapRef} from 'vue';
 import axios from 'axios';
 import {message} from 'ant-design-vue';
 import store from "@/store";
 import router from "@/router";
+import {RuleObject, ValidateErrorEntity} from 'ant-design-vue/es/form/interface'
 
 declare let hexMd5: any;
 declare let KEY: any;
 
+interface LoginUser {
+  loginName: string;
+  password: string;
+}
 
 export default defineComponent({
   name: 'the-header',
   setup() {
+
+
     // 登录后保存
     const user = computed(() => store.state.user);
 
     // 用来登录
-    const loginUser = ref({
+    const formRef = ref();
+    const loginUser: UnwrapRef<LoginUser> = reactive({
       loginName: "test",
       password: "test123"
     });
+    // const loginUser = ref({
+    //   loginName: "test",
+    //   password: "test123"
+    // });
     const loginModalVisible = ref(false);
     const loginModalLoading = ref(false);
     const showLoginModal = () => {
       loginModalVisible.value = true;
     };
 
+    // 校验登录名
+    let validateName = async (rule: RuleObject, value: string) => {
+      if (value === '') {
+        return Promise.reject('请输入登录名');
+      } else {
+        // if (value === '') {
+        //   return Promise.reject('Please input the password');
+        // }
+        return Promise.resolve();
+      }
+    };
+
+    // 校验密码
+    let validatePass = async (rule: RuleObject, value: string) => {
+      if (value === '') {
+        return Promise.reject('请输入密码');
+      } else {
+        if (!/^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,32}$/.test(value)) {
+          return Promise.reject('请输入符合规则的密码');
+        }
+        return Promise.resolve();
+      }
+    };
+
+    // 定义校验规则
+    const rules = {
+      loginName: [{required: true, validator: validateName, trigger: 'change'}],
+      password: [{required: true, validator: validatePass, trigger: 'change'}],
+    }
+
     // 登录
     const login = () => {
-      console.log("开始登录");
-      loginModalLoading.value = true;
-      loginUser.value.password = hexMd5(loginUser.value.password + KEY);
-      axios.post('/user/login', loginUser.value).then((response) => {
-        loginModalLoading.value = false;
-        const data = response.data;
-        if (data.success) {
-          loginModalVisible.value = false;
-          message.success("登录成功！");
-          store.commit("setUser", data.content);
-        } else {
-          message.error(data.message);
-        }
-      });
+      formRef.value
+          .validate()
+          .then(() => {
+            console.log("开始登录");
+            loginModalLoading.value = true;
+            loginUser.password = hexMd5(loginUser.password + KEY);
+            axios.post('/user/login', loginUser).then((response) => {
+              loginModalLoading.value = false;
+              const data = response.data;
+              if (data.success) {
+                loginModalVisible.value = false;
+                message.success("登录成功！");
+                store.commit("setUser", data.content);
+              } else {
+                message.error(data.message);
+              }
+            });
+          })
+          .catch((error: ValidateErrorEntity<LoginUser>) => {
+            console.error(error);
+          });
     };
 
     // 退出登录
@@ -116,6 +167,9 @@ export default defineComponent({
         if (data.success) {
           message.success("退出登录成功！");
           store.commit("setUser", {});
+          // 清空表单项
+          formRef.value.resetFields();
+
           // 退出登录后跳回到首页
           router.push('/');
         } else {
@@ -132,6 +186,9 @@ export default defineComponent({
       login,
       user,
       logout,
+
+      formRef,
+      rules,
     }
   }
 });
