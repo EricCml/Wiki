32a6b0965d8659d2269b648e1030941baa54776b
EricCml <826288248@qq.com>
2021-10-13 13:21:18 +0800

    9-11 前后端增加登录拦截 修复BUG：前后端存储token，统一用String类型

--- src/main/java/com/jiawa/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/jiawa/wiki/config/SpringMvcConfig.java
@@ -1,20 +1,29 @@
-//package com.jiawa.wiki.config;
-//
-//import com.jiawa.wiki.interceptor.LogInterceptor;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//import javax.annotation.Resource;
-//
-//@Configuration
-//public class SpringMvcConfig implements WebMvcConfigurer {
-//
-//    @Resource
-//    LogInterceptor logInterceptor;
-//
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(logInterceptor)
-//                .addPathPatterns("/**").excludePathPatterns("/login");  // addPathPatterns()，要拦截请求;excludePathPatterns()，排除请求，不拦截
-//    }
-//}
+package com.jiawa.wiki.config;
+
+import com.jiawa.wiki.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LoginInterceptor loginInterceptor;
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/test/**",
+                        "/redis/**",
+                        "/user/login",
+                        "/category/all",
+                        "/ebook/list",
+                        "/doc/all/**",
+                        "/doc/find-content/**"
+                );
+    }
+}
