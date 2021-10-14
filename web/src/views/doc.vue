cb49beb192da598193bbec50bd02835c752902b3
EricCml <826288248@qq.com>
2021-10-14 20:18:47 +0800

    10-2 文档页面显示文档阅读数点赞数

--- web/src/views/doc.vue
+++ web/src/views/doc.vue
@@ -15,6 +15,14 @@
           </a-tree>
         </a-col>
         <a-col :span="18">
+          <div>
+            <h2>{{ doc.name }}</h2>
+            <div>
+              <span>阅读数：{{ doc.viewCount }}</span> &nbsp; &nbsp;
+              <span>点赞数：{{ doc.voteCount }}</span>
+            </div>
+            <a-divider style="height: 2px; background-color: #9999cc"/>
+          </div>
           <div class="wangeditor" :innerHTML="html"></div>
         </a-col>
       </a-row>
@@ -37,6 +45,9 @@ export default defineComponent({
     const html = ref();
     const defaultSelectedKeys = ref();
     defaultSelectedKeys.value = [];
+    // 当前选中的文档
+    const doc = ref();
+    doc.value = {};
 
     /**
      * 一级文档树，children属性就是二级文档
@@ -81,6 +92,8 @@ export default defineComponent({
           if (Tool.isNotEmpty(level1)) {
             defaultSelectedKeys.value = [level1.value[0].id];
             handleQueryContent(level1.value[0].id);
+            // 初始显示文档信息
+            doc.value = level1.value[0];
           }
         } else {
           message.error(data.message);
@@ -88,9 +101,14 @@ export default defineComponent({
       });
     };
 
+    // 选择文档
     const onSelect = (selectedKeys: any, info: any) => {
       console.log('selected', selectedKeys, info);
       if (Tool.isNotEmpty(selectedKeys)) {
+        // 选中某一节点时，加载该节点的文档信息
+        doc.value = info.selectedNodes[0].props;
+        // 实时更新文档阅读数
+        doc.value.viewCount++;
         // 加载内容
         handleQueryContent(selectedKeys[0]);
       }
@@ -106,6 +124,7 @@ export default defineComponent({
       html,
       onSelect,
       defaultSelectedKeys,
+      doc,
     }
   }
 });
