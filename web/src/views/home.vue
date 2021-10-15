8325f48289a7670905494328f7cfdcd650c0a9f5
EricCml <826288248@qq.com>
2021-10-15 22:35:01 +0800

    10-6 首页显示电子书信息：文档数、阅读数、点赞数

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -32,9 +32,17 @@
         <template #renderItem="{ item }">
           <a-list-item key="item.name">
             <template #actions>
-              <span v-for="{ type, text } in actions" :key="type">
-                <component v-bind:is="type" style="margin-right: 8px"/>
-                {{ text }}
+              <span>
+                <component v-bind:is="'FileOutlined'" style="margin-right: 8px"/>
+                {{ item.docCount }}
+              </span>
+              <span>
+                <component v-bind:is="'UserOutlined'" style="margin-right: 8px"/>
+                {{ item.viewCount }}
+              </span>
+              <span>
+                <component v-bind:is="'LikeOutlined'" style="margin-right: 8px"/>
+                {{ item.voteCount }}
               </span>
             </template>
             <a-list-item-meta :description="item.description">
@@ -152,11 +160,11 @@ export default defineComponent({
         },
         pageSize: 3,
       },
-      actions: [
-        {type: 'StarOutlined', text: '156'},
-        {type: 'LikeOutlined', text: '156'},
-        {type: 'MessageOutlined', text: '2'},
-      ],
+      // actions: [
+      //   {type: 'StarOutlined', text: '156'},
+      //   {type: 'LikeOutlined', text: '156'},
+      //   {type: 'MessageOutlined', text: '2'},
+      // ],
 
       handleClick,
       level1,
