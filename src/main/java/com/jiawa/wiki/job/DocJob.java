97590f2d8a6500b7e71cf62f592f0d9eaa23ed33
EricCml <826288248@qq.com>
2021-10-15 22:28:02 +0800

    10-6 增加定时器，定时执行电子书信息更新SQL，增加日志

--- src/main/java/com/jiawa/wiki/job/DocJob.java
+++ src/main/java/com/jiawa/wiki/job/DocJob.java
@@ -10,6 +10,7 @@ import javax.annotation.Resource;
 
 @Component
 public class DocJob {
+
     private static final Logger LOG = LoggerFactory.getLogger(DocJob.class);
 
     @Resource
@@ -20,7 +21,10 @@ public class DocJob {
      */
     @Scheduled(cron = "5/30 * * * * ?")
     public void cron() {
+        LOG.info("更新电子书下的文档数据开始");
+        long start = System.currentTimeMillis();
         docService.updateEbookInfo();
+        LOG.info("更新电子书下的文档数据结束，耗时：{}毫秒", System.currentTimeMillis() - start);
     }
 
 }
