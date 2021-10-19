b51c4d6a2a885a743c48ebb356d6259d61e24ee4
EricCml <826288248@qq.com>
2021-10-19 22:11:06 +0800

    10-11 使用RocketMQ解耦点赞通知功能，发送和接收调试成功 rocketmq本地启动：   mqnamesrv.cmd   mqbroker.cmd -n 127.0.0.1:9876 autoCreateTopicEnable=true   使用autoCreateTopicEnable，让客户端可以自由创建topic

--- src/main/java/com/jiawa/wiki/service/DocService.java
+++ src/main/java/com/jiawa/wiki/service/DocService.java
@@ -18,6 +18,7 @@ import com.jiawa.wiki.util.CopyUtil;
 import com.jiawa.wiki.util.RedisUtil;
 import com.jiawa.wiki.util.RequestContext;
 import com.jiawa.wiki.util.SnowFlake;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
@@ -50,6 +51,9 @@ public class DocService {
 
     @Resource
     public WsService wsService;
+
+    @Resource
+    private RocketMQTemplate rocketMQTemplate;
 
     /**
      * 查询文档基本信息
@@ -176,7 +180,8 @@ public class DocService {
         // 推送消息
         Doc docDb = docMapper.selectByPrimaryKey(id);
         String logId = MDC.get("LOG_ID");
-        wsService.sendInfo("【" + docDb.getName() + "】被点赞！", logId);
+        // wsService.sendInfo("【" + docDb.getName() + "】被点赞！", logId);
+        rocketMQTemplate.convertAndSend("VOTE_TOPIC", "【" + docDb.getName() + "】被点赞！");
     }
 
     /**
