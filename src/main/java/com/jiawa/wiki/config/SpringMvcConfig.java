10df3ca077ffc267c9cab328ec05a83bd4c80be4
EricCml <826288248@qq.com>
2021-10-23 16:14:43 +0800

    11-6 前端增加数值统计组件，显示统计数值

--- src/main/java/com/jiawa/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/jiawa/wiki/config/SpringMvcConfig.java
@@ -24,7 +24,8 @@ public class SpringMvcConfig implements WebMvcConfigurer {
                         "/ebook/list",
                         "/doc/all/**",
                         "/doc/find-content/**",
-                        "/doc/vote/**"
+                        "/doc/vote/**",
+                        "/ebook-snapshot/**"
                 );
     }
 }
