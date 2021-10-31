26aec76d79a27a59f3eb70b5f1c572d85b46b439
EricCml <826288248@qq.com>
2021-10-31 11:00:27 +0800

    11-9 关于我们文案修改

--- web/src/views/about.vue
+++ web/src/views/about.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="about">
-    <h1>关于我们</h1>
-  </div>
+  <a-layout>
+    <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
+      <h1>我是蔡孟栾，来自湖北武汉，现为国防科技大学计算机学院在读硕士。</h1>
+    </a-layout-content>
+  </a-layout>
 </template>
