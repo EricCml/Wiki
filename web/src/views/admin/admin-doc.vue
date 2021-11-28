4985abc44096be2c9b593e1cce0363a8beb43dc4
EricCml <826288248@qq.com>
2021-11-28 15:48:42 +0800

    富文本框中显示上传图片按钮，转成Base64存储，同时也支持拖拽图片

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -207,6 +207,11 @@ export default defineComponent({
     const modalLoading = ref(false);
     const editor = new E('#content');
     editor.config.zIndex = 0;
+    // 显示上传图片按钮，转成Base64存储，同时也支持拖拽图片
+    // 上传图片文档：https://www.wangeditor.com/doc/pages/07-%E4%B8%8A%E4%BC%A0%E5%9B%BE%E7%89%87/01-%E9%85%8D%E7%BD%AE%E6%9C%8D%E5%8A%A1%E7%AB%AF%E6%8E%A5%E5%8F%A3.html
+    // 上传视频文档：https://www.wangeditor.com/doc/pages/07-%E4%B8%8A%E4%BC%A0%E8%A7%86%E9%A2%91/01-%E9%85%8D%E7%BD%AE%E6%9C%8D%E5%8A%A1%E7%AB%AF%E6%8E%A5%E5%8F%A3.html
+    editor.config.uploadImgShowBase64 = true;
+
 
     const handleSave = () => {
       modalLoading.value = true;
