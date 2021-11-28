f51fc7820574d98b516f734bedee1c35b9f4a217
EricCml <826288248@qq.com>
2021-11-28 10:48:19 +0800

    只有admin用户可以增删改 增加LoginUserContext，在当前线程的任意地方，可以通过该类获取登录信息

--- src/main/java/com/jiawa/wiki/interceptor/ActionInterceptor.java
+++ src/main/java/com/jiawa/wiki/interceptor/ActionInterceptor.java
@@ -2,6 +2,8 @@ package com.jiawa.wiki.interceptor;
 
 import com.alibaba.fastjson.JSONObject;
 import com.jiawa.wiki.resp.CommonResp;
+import com.jiawa.wiki.resp.UserLoginResp;
+import com.jiawa.wiki.util.LoginUserContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -25,6 +27,13 @@ public class ActionInterceptor implements HandlerInterceptor {
         // OPTIONS请求不做校验,
         // 前后端分离的架构, 前端会发一个OPTIONS请求先做预检, 对预检请求不做校验
         if ("OPTIONS".equals(request.getMethod().toUpperCase())) {
+            return true;
+        }
+
+        // 判断用户，特定用户不拦截
+        UserLoginResp userLoginResp = LoginUserContext.getUser();
+        if ("admin".equals(userLoginResp.getLoginName())) {
+            // admin用户不拦截
             return true;
         }
 
--- src/main/java/com/jiawa/wiki/interceptor/LoginInterceptor.java
+++ src/main/java/com/jiawa/wiki/interceptor/LoginInterceptor.java
@@ -1,5 +1,8 @@
 package com.jiawa.wiki.interceptor;
 
+import com.alibaba.fastjson.JSON;
+import com.jiawa.wiki.resp.UserLoginResp;
+import com.jiawa.wiki.util.LoginUserContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -54,6 +57,7 @@ public class LoginInterceptor implements HandlerInterceptor {
             return false;
         } else {
             LOG.info("已登录：{}", object);
+            LoginUserContext.setUser(JSON.parseObject((String) object, UserLoginResp.class));
             return true;
         }
     }
