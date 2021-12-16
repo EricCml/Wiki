be6e429e37cad99d63e60dbb5cd2e6653b7ddcd1
EricCml <826288248@qq.com>
2021-12-16 21:05:50 +0800

    添加备案信息

--- web/src/components/the-footer.vue
+++ web/src/components/the-footer.vue
@@ -1,10 +1,60 @@
 <template>
   <a-layout-footer style="text-align: center">
-    边城知识库<span v-show="user.id">，欢迎：{{ user.name }}</span>
-    <br>
-    ICP证 : <a href="https://beian.miit.gov.cn/" target="_blank">湘ICP备2021019370号-1</a>
-    <br>
-    Copyright © 2021 BianChengCS , All rights reserved.
+    <div>
+      边城知识库<span v-show="user.id">，欢迎：{{ user.name }}</span>
+    </div>
+    <div>
+      Copyright © 2021 BianChengCS , All rights reserved.
+    </div>
+
+    <div style="width:500px; margin:0 auto; padding:20px 0; text-align:center;">
+      <span>
+        <img
+            src="../assets/gongan.png"
+            style="height:20px; width:20px;"/>
+        <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=42011202002035"
+           style="display:inline-block; text-decoration:none; height:20px; line-height:20px;">
+            <p style="height:20px; line-height:20px; color:#939393;">
+              鄂公网安备42011202002035号
+            </p>
+        </a>
+      </span>
+      <span>  •  </span>
+      <span>
+        <a target="_blank" href="https://beian.miit.gov.cn/"
+           style="display:inline-block; text-decoration:none; height:20px; line-height:20px;">
+            <p style="height:20px; line-height:20px; color:#939393;">
+              湘ICP备2021019370号-1
+            </p>
+        </a>
+      </span>
+    </div>
+
+    <!--    <div style="width:400px;margin:0 auto; padding:20px 0;">-->
+    <!--      <span>-->
+    <!--        <a target="_blank"-->
+    <!--           href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=42011202002035"-->
+    <!--           class="beian">-->
+    <!--          <img-->
+    <!--              src="../../dist/image/gongan.png"-->
+    <!--              style="float:left;"/>-->
+    <!--          <p class="beian"-->
+    <!--             style="float:left;">-->
+    <!--            鄂公网安备42011202002035号-->
+    <!--          </p>-->
+    <!--        </a>-->
+    <!--      </span>-->
+    <!--      <span>-->
+    <!--        &lt;!&ndash;        <p class="beian"> • </p>&ndash;&gt;-->
+    <!--        <a target="_blank"-->
+    <!--           href="https://beian.miit.gov.cn/"-->
+    <!--           class="beian">-->
+    <!--          <p class="beian">-->
+    <!--            湘ICP备2021019370号-1-->
+    <!--          </p>-->
+    <!--        </a>-->
+    <!--      </span>-->
+    <!--    </div>-->
   </a-layout-footer>
 </template>
 
@@ -69,3 +119,19 @@ export default defineComponent({
   }
 });
 </script>
+
+<!--<style>-->
+<!--.beian p {-->
+<!--  height: 20px;-->
+<!--  line-height: 20px;-->
+<!--  margin: 0px 0px 0px 5px;-->
+<!--  color: #939393;-->
+<!--}-->
+
+<!--.beian a {-->
+<!--  display: inline-block;-->
+<!--  text-decoration: none;-->
+<!--  height: 20px;-->
+<!--  line-height: 20px;-->
+<!--}-->
+<!--</style>-->
